04b90d0a884e23d2a183cfbdb2d1a90931fa2f3d
Sebabin <sebastiancrespin279@gmail.com>
2022-01-04 19:49:38 -0700

    have routes updated to return newly created parties table information with the query of candidates or a single candidate

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ db.connect(function(err){
 
 app.get('/api/candidates',(req,res) => {
 // get all candidates
-const sql = `SELECT * FROM candidates`;
+const sql = `SELECT candidates.*,parties.name
+AS party_name
+FROM candidates
+LEFT JOIN parties
+ON candidates.party_id = parties.id`;
   db.query(sql, (err,rows) => {
    if(err) {
        res.statusCode(500).json({error: err.message});
@@ -46,8 +50,16 @@ const sql = `SELECT * FROM candidates`;
 
 // get candidate by id
 app.get('/api/candidate/:id',(req,res) => {
-    const sql = `SELECT * FROM  candidates WHERE id = ?`;
+   /*sql query*/
+    const sql = `SELECT candidates.*,parties.name
+    AS party_name
+    FROM candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id
+    WHERE candidates.id = ?`;
+    /*id sent from client */
     const params = [req.params.id];
+    /**query to sql database */
   db.query(sql,params,(err,row) => {
     if(err) {
       res.statusCode(400).json({error: error.message});
