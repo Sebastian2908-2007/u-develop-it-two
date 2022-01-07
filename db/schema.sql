8a43e8a1c698c7f7161f8ec56251887ab2e29002
Sebabin <sebastiancrespin279@gmail.com>
2022-01-06 17:27:55 -0700

    have a votes table set up in schema and all constraints working proper

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,9 @@
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 /*Table candidates must be dropped first because of the FK CONSTRAINT that requires the parties table to exist*/
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
-CREATE TABLE voters (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
 
 /* parties must be created before candidates because of the FK CONSTRAINT that relies on the parties table */
   CREATE TABLE parties(
@@ -26,3 +20,26 @@ party_id INTEGER,
 industry_connected  BOOLEAN NOT NULL,
 CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
+
+/*voters table*/
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+/*votes table*/
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fK_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+
+);
+
